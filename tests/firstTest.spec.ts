bf7019510e0ed97ad2a948281f614d88ad2c7e57
Reuben Ellis <ethriel3695@gmail.com>
2024-01-30 07:20:00 -0700

    get correct directory for playwright tests

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -5,3 +5,17 @@ test("screenshot homepage", async ({ page }) => {
   await page.goto("https://www.chromatic.com/start");
   await argosScreenshot(page, "homepage");
 });
+
+test("screenshot homepage mobile", async ({ page }) => {
+  await page.setViewportSize({
+    width: 390,
+    height: 844,
+  });
+  await page.goto("https://www.chromatic.com/start");
+  await argosScreenshot(page, "homepage");
+});
+
+test("screenshot google", async ({ page }) => {
+  await page.goto("https://www.google.com");
+  await argosScreenshot(page, "homepage");
+});
